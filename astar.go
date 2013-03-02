fb2bd35c2b5c6e2d1b17498250522eef8bc895b6
Christoph <christoph@orion.(none)>
2013-03-02 21:49:15 +0100

    Initial commit

---
+++ astar.go
@@ -0,0 +1,166 @@
+// Copyright (c) 2011, Christoph Schunk
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the author nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package astar
+
+import (
+	"container/heap"
+)
+
+type HeuristicCostEstimateFunc func(node, goal Node) float32
+
+type none struct {
+}
+
+type Node interface {
+	NumNeighbours() int
+	Neighbour(i int) Node
+	Cost(i int) float32
+}
+
+type nodeElement struct {
+	F        float32
+	G        float32
+	H        float32
+	Node     Node
+	cameFrom *nodeElement
+	//Optimization: use Closed bool instead of an hash?
+}
+
+type nodeElementPool []nodeElement
+type nodeElementHeap []*nodeElement
+
+type AStar struct {
+	costEstimateFunc HeuristicCostEstimateFunc
+	elmentPool       nodeElementPool
+	openList         nodeElementHeap
+	closedList       map[Node]none
+	path             []Node
+}
+
+func (p *nodeElementPool) NewElement(f, g, h float32, node Node, cameFrom *nodeElement) *nodeElement {
+	(*p) = append(*p, nodeElement{f, g, h, node, cameFrom})
+	return &(*p)[len(*p)-1]
+}
+
+func (ol *nodeElementHeap) Len() int {
+	return len(*ol)
+}
+
+func (ol *nodeElementHeap) Less(i, j int) bool {
+	return (*ol)[i].F < (*ol)[j].F
+}
+
+func (ol *nodeElementHeap) Swap(i, j int) {
+	(*ol)[i], (*ol)[j] = (*ol)[j], (*ol)[i]
+}
+
+func (ol *nodeElementHeap) Push(e interface{}) {
+	(*ol) = append(*ol, e.(*nodeElement))
+}
+
+func (ol *nodeElementHeap) Pop() interface{} {
+	e := (*ol)[len(*ol)-1]
+	(*ol) = (*ol)[:len(*ol)-1]
+	return e
+}
+
+func New(costEstimateFunc HeuristicCostEstimateFunc) *AStar {
+	return &AStar{
+		costEstimateFunc,
+		make(nodeElementPool, 0, 1024),
+		make(nodeElementHeap, 0, 256),
+		make(map[Node]none),
+		make([]Node, 0, 128)}
+}
+
+func (as *AStar) Path() []Node {
+	return as.path
+}
+
+func (as *AStar) reset() {
+	as.elmentPool = as.elmentPool[:0]
+	as.openList = as.openList[:0]
+	as.closedList = make(map[Node]none)
+	as.path = as.path[:0]
+}
+
+func (as *AStar) Find(start, goal Node) bool {
+	as.reset()
+	g := float32(0)
+	h := as.costEstimateFunc(start, goal)
+	heap.Push(&as.openList, as.elmentPool.NewElement(g+h, g, h, start, nil))
+	for len(as.openList) > 0 {
+		currentNodeElement := heap.Pop(&as.openList).(*nodeElement)
+		if currentNodeElement.Node == goal {
+			as.reconstructPath(currentNodeElement)
+			return true
+		}
+		as.expandNode(currentNodeElement, goal)
+		as.closedList[currentNodeElement.Node] = none{}
+	}
+	return false
+}
+
+func (as *AStar) expandNode(currentNodeElement *nodeElement, goal Node) {
+	numNeigbours := currentNodeElement.Node.NumNeighbours()
+	for i := 0; i < numNeigbours; i++ {
+		successorNode := currentNodeElement.Node.Neighbour(i)
+		if _, ok := as.closedList[successorNode]; ok {
+			continue;
+		}
+		t := currentNodeElement.G + currentNodeElement.Node.Cost(i)
+		if index, ok := as.inOpenList(successorNode); ok {
+			g := as.openList[index].G
+			if t < g {
+				g, h := t, as.costEstimateFunc(successorNode, goal)
+				elem := heap.Remove(&as.openList, index).(*nodeElement)
+				elem.G, elem.H, elem.F = g, h, g + h
+				elem.cameFrom = currentNodeElement
+				heap.Push(&as.openList, elem)
+			}
+		} else {
+			g, h := t, as.costEstimateFunc(successorNode, goal)
+			heap.Push(&as.openList, as.elmentPool.NewElement(g+h, g, h, successorNode, currentNodeElement))
+		}
+	}
+}
+
+func (as *AStar) reconstructPath(currentNodeElement *nodeElement) {
+	nextNode := currentNodeElement
+	for nextNode != nil {
+		as.path = append(as.path, nextNode.Node)
+		nextNode = nextNode.cameFrom
+	}
+}
+
+func (as *AStar) inOpenList(n Node) (index int, ok bool) {
+	for i, node := range as.openList {
+		if n == node.Node {
+			return i, true
+		}
+	}
+	return -1, false
+}
---
+++ astar_test.go
@@ -0,0 +1,126 @@
+// Copyright (c) 2011, Christoph Schunk
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the author nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package astar
+
+import (
+	"testing"
+	"math"
+)
+
+var (
+	costs = []float32{1, 1.141, 1.0, 1.141, 1.0, 1.141, 1.0, 1.141}
+	dirx = []int{ 0,  1, 1, 1, 0, -1, -1, -1}
+	diry = []int{-1, -1, 0, 1, 1,  1,  0, -1}
+)
+
+type MNode struct {
+	m *Map
+	x, y int
+	cost float32
+}
+
+type Map struct {
+	nodes []MNode
+	w, h  int
+}
+
+func NewMap(w, h int) *Map {
+	m := &Map{make([]MNode, w*h), w, h}
+	for i, _ := range m.nodes {
+		n := &m.nodes[i]
+		n.m = m
+		n.cost = 1.0
+		n.x = i % w
+		n.y = i / w
+	}
+	return m
+}
+
+func (m *Map) Node(x, y int) *MNode {
+	if x < 0 {
+		x += m.w
+	} else if x >= m.w {
+		x -= m.w
+	}
+	if y < 0 {
+		y += m.h
+	} else if y >= m.h {
+		y -= m.h
+	}
+	return &m.nodes[x+y*m.w]
+}
+
+func (n *MNode) NumNeighbours() int {
+	return 8
+}
+
+func (n *MNode) Cost(i int) float32 {
+	return n.m.Node(n.x+dirx[i], n.y+diry[i]).cost * costs[i]
+}
+
+func (n *MNode) Neighbour(i int) Node {
+	return n.m.Node(n.x+dirx[i], n.y+diry[i])
+}
+
+func Heuristic(node, goal Node) float32 {
+	s := node.(*MNode)
+	g := goal.(*MNode)
+	dx := (s.x - g.x)
+	dy := (s.y - g.y)
+	return float32(math.Sqrt(float64(dx*dx + dy*dy)))
+}
+
+type pos struct {
+	x, y int
+}
+
+type pathTest struct {
+	start, end pos
+	path []pos
+}
+
+var pathTests = []pathTest {
+	{pos{0, 0}, pos{0, 0}, []pos{{0, 0}}},
+	{pos{0, 0}, pos{1, 1}, []pos{{1, 1}, {0, 0}}},
+	{pos{1, 1}, pos{5, 1}, []pos{{5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}}},
+}
+
+func TestAStar(t *testing.T) {
+	as := New(Heuristic)
+	m  := NewMap(32, 32)
+	for i, _ := range pathTests {
+		tst := &pathTests[i]
+		if !as.Find(m.Node(tst.start.x, tst.start.y), m.Node(tst.end.x, tst.end.y)) {
+			t.Fatal("path not found")
+		}
+		for i, n := range as.Path() {
+			mn := n.(*MNode)
+			if mn.x != tst.path[i].x || mn.y != tst.path[i].y {
+				t.Fatalf("wrong path at %d: node: %v path node: %v", i, mn, tst.path[i])
+			}
+		}
+	}
+}
